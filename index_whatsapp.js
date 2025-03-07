0c1d9c06fcfe44b56a36f0f848c6bdd1c3f0f7ce
Erdana <tursunov.erdana07@mail.ru>
2025-03-07 14:24:15 +0500

    whatsapp auth 2

--- index_whatsapp.js
+++ index_whatsapp.js
@@ -31,6 +31,12 @@ app.use("/ai", ai_router);
 // Инициализация WhatsApp клиента
 const client = new Client({
   authStrategy: new LocalAuth(),
+  puppeteer: {
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome-stable",
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
 });
 
 let lastQrCode = null;
@@ -53,7 +59,9 @@ client.on("message", async (message) => {
   if (message.isGroupMsg || message.fromMe || message.author) return;
 
   console.log(
-    `📩 [${new Date().toLocaleTimeString()}] Сообщение от ${message.from}: ${message.body}`
+    `📩 [${new Date().toLocaleTimeString()}] Сообщение от ${message.from}: ${
+      message.body
+    }`
   );
 
   try {
